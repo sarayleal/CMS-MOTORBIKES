1902c31588c2ca941ba0ce1a289fd9cc953ad636
PabloTei <pteijeirolopez@gmail.com>
2023-02-23 23:13:16 +0100

    map-filter

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,44 @@
 import './Home.css';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+//import { useDebounce } from 'use-debounce';
+import MotoCard from '../../components/MotoCard/Motocard';
+
 const Home = () => {
+  const [motos, setMotos] = useState([]);
+  const [filter, setFilter] = useState([]);
+  //const debounceValue = useDebounce(filter, 1000);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    axios.get('https://63ed61e93d9c852c3f59f7e0.mockapi.io/motos').then((res) => {
+      setMotos(res.data);
+      setFilter(res.data);
+      setLoaded(true);
+    });
+  }, []);
+
+  const filterFunction = (value) => {
+    const arrayFilter = motos.filter((moto) => moto.name.toLowerCase().includes(value));
+    setFilter(arrayFilter);
+  };
+
   return (
     <main className="Home">
-      <h2>Home</h2>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(ev) => filterFunction(ev.target.value.toLowerCase())}
+      />
+      <div>
+        {loaded ? (
+          filter.map((moto) => <MotoCard key={moto.id} moto={moto} />)
+        ) : (
+          <p>Cargando...</p>
+        )}
+      </div>
     </main>
   );
 };
