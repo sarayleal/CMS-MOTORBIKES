fb43d88344dc6df74157f92cfbb13f3a152adb42
PabloTei <pteijeirolopez@gmail.com>
2023-02-24 15:48:59 +0100

    fetch-create

--- src/Pages/Company/Company.jsx
+++ src/Pages/Company/Company.jsx
@@ -2,28 +2,31 @@ import './Company.css';
 
 import { useEffect, useState } from 'react';
 
+import InputCreate from '../../components/InputCreate';
 /* import { UserContext } from '../../components/context/userContext'; */
 import MotoCard from '../../components/MotoCard/MotoCard';
+import SelectCreate from '../../components/SelectCreate';
+import { carnet, fuel, type, typesText } from '../../data/data.js';
 
 const Company = () => {
   //GET
   const [motos, setMotos] = useState([]);
   const [loaded, setLoaded] = useState(false);
-  // const [error, setError] = useState(null);
-  // const [newMoto, setNewMoto] = useState({
-  //   image: '',
-  //   brand: '',
-  //   name: '',
-  //   year: '',
-  //   type: '',
-  //   km: '',
-  //   cv: '',
-  //   price: '',
-  //   cc: '',
-  //   carnet: '',
-  //   company: '',
-  //   fuel: '',
-  // });
+  const [error, setError] = useState(null);
+  const [newMoto, setNewMoto] = useState({
+    image: '',
+    brand: '',
+    name: '',
+    year: '',
+    type: '',
+    km: '',
+    cv: '',
+    price: '',
+    cc: '',
+    carnet: '',
+    company: '',
+    fuel: '',
+  });
   const getMotos = async () => {
     const res = await fetch('https://63ed61e93d9c852c3f59f7e0.mockapi.io/motos');
     const data = await res.json();
@@ -31,36 +34,36 @@ const Company = () => {
     setLoaded(true);
   };
   //CREATE
-  // const createMotos = (ev) => {
-  //   ev.preventDefault();
-  //   if (
-  //     !newMoto.image ||
-  //     !newMoto.brand ||
-  //     !newMoto.name ||
-  //     !newMoto.year ||
-  //     !newMoto.type ||
-  //     !newMoto.km ||
-  //     !newMoto.cv ||
-  //     !newMoto.price ||
-  //     !newMoto.cc ||
-  //     !newMoto.carnet ||
-  //     !newMoto.company ||
-  //     !newMoto.fuel
-  //   ) {
-  //     setError('faltan datos');
-  //   } else {
-  //     setError(null);
-  //     fetch('https://63ed61e93d9c852c3f59f7e0.mockapi.io/motos', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(newMoto),
-  //     }).then((res) => {
-  //       getMotos();
-  //     });
-  //   }
-  // };
+  const createMotos = (ev) => {
+    ev.preventDefault();
+    if (
+      !newMoto.image ||
+      !newMoto.brand ||
+      !newMoto.name ||
+      !newMoto.year ||
+      !newMoto.type ||
+      !newMoto.km ||
+      !newMoto.cv ||
+      !newMoto.price ||
+      !newMoto.cc ||
+      !newMoto.carnet ||
+      !newMoto.company ||
+      !newMoto.fuel
+    ) {
+      setError('faltan datos');
+    } else {
+      setError(null);
+      fetch('https://63ed61e93d9c852c3f59f7e0.mockapi.io/motos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMoto),
+      }).then(() => {
+        getMotos();
+      });
+    }
+  };
 
   useEffect(() => {
     getMotos();
@@ -69,11 +72,43 @@ const Company = () => {
   /*  const { user } = useContext(UserContext); */
   return (
     <main className="Company">
-      {loaded ? (
-        motos.map((moto) => <MotoCard key={moto.id} moto={moto} />)
-      ) : (
-        <p>Cargando...</p>
-      )}
+      <div>
+        {loaded ? (
+          motos.map((moto) => <MotoCard key={moto.id} moto={moto} />)
+        ) : (
+          <p>Cargando...</p>
+        )}
+      </div>
+      <div>
+        <form onSubmit={(ev) => createMotos(ev)}>
+          {typesText.map((info) => (
+            <InputCreate
+              type={info}
+              key={info}
+              action={(ev) => {
+                setNewMoto({ ...newMoto, [info]: ev.target.value }), console.log(newMoto);
+              }}
+            />
+          ))}
+          <SelectCreate
+            options={fuel}
+            action={(ev) => setNewMoto({ ...newMoto, fuel: ev.target.value })}
+          />
+          <SelectCreate
+            options={carnet}
+            action={(ev) => setNewMoto({ ...newMoto, carnet: ev.target.value })}
+          />
+          <SelectCreate
+            options={type}
+            action={(ev) => setNewMoto({ ...newMoto, type: ev.target.value })}
+          />
+
+          <button onClick={() => console.log(newMoto)} type="submit">
+            Create
+          </button>
+        </form>
+        {error && <h3>{error}</h3>}
+      </div>
     </main>
   );
 };
--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,29 @@
-// const typesSelect = [
-//   'image',
-//   'brand',
-//   'name',
-//   'year',
-//   'km',
-//   'cv',
-//   'price',
-//   'cc',
-//   'company',
-// ];
+export const typesText = [
+  'image',
+  'brand',
+  'name',
+  'year',
+  'km',
+  'cv',
+  'price',
+  'cc',
+  'company',
+];
+
+export const typesSelect = ['carnet', 'fuel', 'type'];
+
+export const fuel = ['gas', 'diesel', 'electric'];
+
+export const carnet = ['a1', 'a2', 'b', 'a'];
+
+export const type = [
+  'naked',
+  'custom',
+  'scooter',
+  'sport',
+  'supermotard',
+  'trail',
+  'enduro',
+  'cross',
+  'infantil',
+];
