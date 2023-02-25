a14f70167b9623df63aa52478947d267ac8b2a2c
Laura VL <laura_villarreal.l@hotmail.com>
2023-02-25 21:26:12 +0100

    motocard

--- src/components/MotoCard/MotoCard.jsx
+++ src/components/MotoCard/MotoCard.jsx
@@ -4,18 +4,38 @@ import { Link } from 'react-router-dom';
 
 const MotoCard = ({ moto }) => {
   return (
-    <figure>
-      <img src={moto.image} alt={moto.name} />
-      <h3>{moto.name}</h3>
-      <h3>{moto.year}</h3>
-      <h3>{moto.fuel}</h3>
-      <h3>{moto.type}</h3>
-      <h3>{moto.carnet}</h3>
-      <h3>{moto.company}</h3>
-      <button>
-        <Link to={`/Home/${moto.id}`}>More Details</Link>
-      </button>
-    </figure>
+    <div className="card">
+      <div className="content">
+        <div className="thumb" style={{ backgroundImage: `url(${moto.image})` }}></div>
+        <h3>{moto.name}</h3>
+        <div className="line"></div>
+        <div className="specifications">
+          <span>
+            {moto.year}
+            <p>Year</p>
+          </span>
+          <span>
+            {moto.fuel}
+            <p>Fuel</p>
+          </span>
+          <span>
+            {moto.type}
+            <p>Type</p>
+          </span>
+          <span>
+            {moto.carnet}
+            <p>Carnet</p>
+          </span>
+          <span>
+            {moto.company}
+            <p>Company</p>
+          </span>
+        </div>
+        <button>
+          <Link to={`/Home/${moto.id}`}>More Details</Link>
+        </button>
+      </div>
+    </div>
   );
 };
 
