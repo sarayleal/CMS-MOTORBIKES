d3df4601f9238b3ab95ca2000d0de339a54162a1
Laura VL <laura_villarreal.l@hotmail.com>
2023-02-26 00:09:46 +0100

    fix motocard

--- src/components/MotoCard/MotoCard.jsx
+++ src/components/MotoCard/MotoCard.jsx
@@ -4,29 +4,29 @@ import { Link } from 'react-router-dom';
 
 const MotoCard = ({ moto }) => {
   return (
-    <div className="card">
+    <div className="cardMoto">
       <div className="content">
         <div className="thumb" style={{ backgroundImage: `url(${moto.image})` }}></div>
         <h3>{moto.name}</h3>
         <div className="line"></div>
         <div className="specifications">
-          <span>
+          <span className="specification">
             {moto.year}
             <p>Year</p>
           </span>
-          <span>
+          <span className="specification">
             {moto.fuel}
             <p>Fuel</p>
           </span>
-          <span>
+          <span className="specification">
             {moto.type}
             <p>Type</p>
           </span>
-          <span>
+          <span className="specification">
             {moto.carnet}
             <p>Carnet</p>
           </span>
-          <span>
+          <span className="specification">
             {moto.company}
             <p>Company</p>
           </span>
