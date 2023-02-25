d484540acf0b4edd1fd1e7a394f0a5745bc71478
PabloTei <pteijeirolopez@gmail.com>
2023-02-25 15:06:46 +0100

    diseño-header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,18 +14,39 @@ const Header = () => {
       <nav>
         <ul>
           <li>
+            <a href="/">
+              <img
+                src="https://res.cloudinary.com/depifliz3/image/upload/v1677328528/samples/Motos/531-5311438_motobike-logo-hobbiesxstyle-logos-para-dibujar-de-motos_hpqcyh.png"
+                alt="logo-cms"
+              />
+            </a>
+          </li>
+          <li>
             <NavLink to="/">Home</NavLink>
           </li>
-          <li>{!user && <NavLink to="/Login">Login</NavLink>}</li>
-          <li>{user && <NavLink to="/Company">Company</NavLink>}</li>
+          {!user && (
+            <li>
+              <NavLink to="/Login">Login</NavLink>
+            </li>
+          )}
+          {user && (
+            <li>
+              <NavLink to="/Company">Company</NavLink>
+            </li>
+          )}
           <li>
             <NavLink to="/About">About</NavLink>
           </li>
+
+          {user && (
+            <li>
+              <button onClick={() => logout()}>Logout</button>
+            </li>
+          )}
           <li>
             <input type="checkbox" className="modeTheme" onChange={() => toggleTheme()} />
           </li>
         </ul>
-        {user && <button onClick={() => logout()}>Logout</button>}
       </nav>
     </header>
   );
